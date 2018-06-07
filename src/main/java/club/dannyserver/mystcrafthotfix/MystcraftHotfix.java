bc55152b5760008c9697155d43d144999e57f912
Chan, Danny <danny50610@yahoo.com.tw>
2018-06-07 22:42:48 +0800

    Init project

---
+++ src/main/java/club/dannyserver/mystcrafthotfix/MystcraftHotfix.java
@@ -0,0 +1,12 @@
+package club.dannyserver.mystcrafthotfix;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = "mystcraft_hotfix",
+        name = "MystcraftHotfix",
+        dependencies = "required-after:forge@[14.23.3.2702,);",
+        serverSideOnly = true
+)
+public class MystcraftHotfix {
+}
